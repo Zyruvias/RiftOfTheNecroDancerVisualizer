551b4e4a28c27bd0814056aee8eeba1f459937ed
Zyruvias <zerianledrias@gmail.com>
2025-03-06 10:11:52 -0500

    Updates

--- data/index.js
+++ data/index.js
@@ -1,51 +1,14 @@
-export const TRACK_LIST = [
-    { value: "DiscoDisaster", label: "Disco Disaster" },
-    { value: "Elusional", label: "Elusional" },
-    { value: "Visualize_Yourself", label: "Visualize Yourself" },
-    { value: "Spookhouse Pop", label: "Spookhouse Pop" },
-    { value: "Om and On", label: "Om And On" },
-    { value: "Morning_Dove", label: "Morning Dove" },
-    { value: "Heph's Mess", label: "Heph's Mess" },
-    { value: "Amalgamaniac", label: "Amalgamaniac" },
-    { value: "Hang Ten Heph", label: "Hang Ten Heph" },
-    { value: "Count_Funkula", label: "Count Funkula" },
-    { value: "Overthinker", label: "Overthinker" },
-    { value: "Cryp2que", label: "Cryp2que" },
-    { value: "Nocturning", label: "Nocturning" },
-    { value: "Glass Cages", label: "Glass Cages" },
-    { value: "Hallow Queen", label: "Hallow Queen" },
-    { value: "Progenitor", label: "Progenitor" },
-    { value: "Matriarch", label: "Matriarch" },
-    { value: "Thunder", label: "Under The Thunder" },
-    { value: "Matron", label: "What's In The Box?" },
-    { value: "Eldritch_House", label: "Eldritch House" },
-    { value: "RAVEvenge", label: "RAVEVENGE" },
-    { value: "Rift Within", label: "Rift Within" },
-    { value: "Suzus Quest", label: "Suzu's Quest" },
-    { value: "Necropolis", label: "Necropolis" },
-    { value: "Baboosh", label: "Baboosh" },
-    { value: "Necro Sonatica", label: "Necro Sonaetica" },
+import { tracks } from "./TrackList.json"
 
-    { value: "Harmonie", label: "She Banned" },
-    { value: "Deep_Blues", label: "King's Ruse" },
-    { value: "BraveTheHarvester", label: "Brave The Harvester" },
-    { value: "Final_Fugue", label: "Final Fugue" },
-
-    { value: "Twombtorial", label: "Twombtorial" },
-    { value: "Portamello", label: "Portamello" },
-
-    { value: "DLCApricot01", label: "Slugger's Refrain" },
-    { value: "DLCApricot02", label: "Got Danged" },
-    { value: "DLCApricot03", label: "Bootus Bleez" },
-]
+export const TRACK_LIST = tracks
 
 // TODO: track files should just be renamed but I got lazy
 export const getTrack = async (track, difficulty) => {
     try {
-        return await import(`./${difficulty.label}-RhythmRift_${track.value}_${difficulty.value}.json`)
+        return await import(`./Charts/${difficulty.label}-RhythmRift_${track.value}_${difficulty.value}.json`)
     } catch (e) {
         console.warn(e)
-        return await import(`./${difficulty.label}-RhythmRift_${track.value}_${difficulty.value}_DoubleSpeed.json`)
+        return await import(`./Charts/${difficulty.label}-RhythmRift_${track.value}_${difficulty.value}_DoubleSpeed.json`)
     }
 }
 
